f30a616e2bede26953069d81b7475382d4b0aad7
Estefania <nialupo87@gmail.com>
2021-01-11 19:36:35 -0500

    working on activity

---
+++ components/HelloBootstrap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloBootstrap = () => {
+    return (
+        <div>
+            <h1>Hellow World</h1>
+        </div>
+    );
+};
+
+export default HelloBootstrap;

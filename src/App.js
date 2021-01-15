16d2492abd8687ddc77027ee13d19f3bcc0ae632
Estefania <nialupo87@gmail.com>
2021-01-15 17:07:51 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import SpongeBobCard from "./components/SpongeBobCard";
-import SquidwardCard from "./components/SquidwardCard";
-import MrKrabsCard from "./components/MrKrabsCard";
-import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
+import FriendCard from "./components/FriendCard"
 
 function App() {
   return (
+    <>
     <Wrapper>
       <Title>Friends List</Title>
-     
-      <FriendCard />
+      <FriendCard
+      name="Squidward"
+      occupation="Cashier"
+      location="An Easter Island Head"
+      image={friends[2].image}
+      />
+
+    <FriendCard
+    
+    name={friends[1].name}
+    occupation={friends[1].occupation}
+    location={friends[1].location}
+    image={friends[1].image}/>
     </Wrapper>
+    </>
   );
 }
 
--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import "./components/FriendCard.css";
+import "./FriendCard.css"
 
-const FriendCard = () => {
+
+const FriendCard = (props) => {
     return (
         <div>
-              <div className="card">
+            
+    <div className="card">
       <div className="img-container">
         <img
-          alt="Squidward"
-          src="https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626"
+          alt={props.name}
+          src={props.image}
         />
       </div>
       <div className="content">
         <ul>
           <li>
-            <strong>Name:</strong> Squidward
+            <strong>Name</strong> {props.name}
           </li>
           <li>
-            <strong>Occupation:</strong> Cashier
+            <strong>Occupation:</strong> {props.occupation}
           </li>
           <li>
-            <strong>Location:</strong> An Easter Island Head
+            <strong>Location:</strong> {props.occupation}
           </li>
         </ul>
       </div>
     </div>
+  
         </div>
     );
 };

fe618e872ddef2f02e1f53436ab60534a693fa97
Estefania <nialupo87@gmail.com>
2021-01-16 10:22:48 -0500

    done

--- src/App.js
+++ src/App.js
@@ -7,70 +7,34 @@ import friends from './friends.json';
 class App extends Component {
 
   state = {
-    friends: "friends"
+    friends: friends,
+  }
+
+  removeFriend= (id) => {
+    // friend.id !== id is an implicint returm
+const newFriends = this.state.friends.filter((friend) => friend.id !== id);
+this.setState({ friends: newFriends })
   }
   render() {
     return (
-      <div>
-         <Wrapper>
+      <Wrapper>
       <Title>Friends List</Title>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
+      {/* THIS IS WHAT LOOPED THROUGH ALL THE CHARACTERS ON FRIEND.JSON USING MAP */}
+      {this.state.friends.map((friend)=>  (<FriendCard
+        name={friend.name}
+        image={friend.image}
+        occupation={friend.occupation}
+        location={friend.location}
+        id={friend.id}
+        handleClick={this.removeFriend}
+      />))}
+
+     
     </Wrapper>
-      </div>
     );
   }
 }
 
 export default App;
 
-// import React from 'react';
 
-// import FriendCard from './components/FriendCard';
-// import Wrapper from './components/Wrapper';
-// import Title from './components/Title';
-// import friends from './friends.json';
-
-// function App() {
-//   return (
-//     <Wrapper>
-//       <Title>Friends List</Title>
-//       <FriendCard
-//         name={friends[0].name}
-//         image={friends[0].image}
-//         occupation={friends[0].occupation}
-//         location={friends[0].location}
-//       />
-//       <FriendCard
-//         name={friends[1].name}
-//         image={friends[1].image}
-//         occupation={friends[1].occupation}
-//         location={friends[1].location}
-//       />
-//       <FriendCard
-//         name={friends[2].name}
-//         image={friends[2].image}
-//         occupation={friends[2].occupation}
-//         location={friends[2].location}
-//       />
-//     </Wrapper>
-//   );
-// }
-
-// export default App;

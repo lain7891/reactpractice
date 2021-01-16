e3d52fd811e25ed798431f59af247acedaa9639e
Estefania <nialupo87@gmail.com>
2021-01-16 10:46:27 -0500

    completed activity

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import React, { Component } from 'react';
-import FriendCard from './components/FriendCard';
-import Wrapper from './components/Wrapper';
-import Title from './components/Title';
-import friends from './friends.json';
+import React from "react";
+import Form from "./components/Form";
 
-class App extends Component {
-
-  state = {
-    friends: friends,
-  }
-
-  removeFriend= (id) => {
-    // friend.id !== id is an implicint returm
-const newFriends = this.state.friends.filter((friend) => friend.id !== id);
-this.setState({ friends: newFriends })
-  }
-  render() {
-    return (
-      <Wrapper>
-      <Title>Friends List</Title>
-      {/* THIS IS WHAT LOOPED THROUGH ALL THE CHARACTERS ON FRIEND.JSON USING MAP */}
-      {this.state.friends.map((friend)=>  (<FriendCard
-        name={friend.name}
-        image={friend.image}
-        occupation={friend.occupation}
-        location={friend.location}
-        id={friend.id}
-        handleClick={this.removeFriend}
-      />))}
-
-     
-    </Wrapper>
-    );
-  }
+function App() {
+  return <Form />;
 }
 
 export default App;
-
-

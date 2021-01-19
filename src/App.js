8a8b5fac7ae74cd23be7a2b88100fbb818e067b3
Estefania <nialupo87@gmail.com>
2021-01-18 20:29:26 -0500

    not quite done, have errors

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Signup from "./pages/Signup";
+import "./App.css";
+// import React, { useState } from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Signup/index.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import Container from "../../components/Container";
+import Col from "../../components/Col";
+import Row from "../../components/Row";
+
+
+const Signup = () => {
+
+  // 1. HOOKS
+  const [state, setState] = useState({
+    userName: "",
+    password: ""
+  });
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="mt-4">
+        <h2>Sign Up</h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <Container className="mt-3 px-5">
+          <Row className="form-group">
+            <Col size="12">
+              <input className="form-control" type="text" placeholder="Username" name="username" 
+              onChange={(e) => setUserName(e.target.value)} />
+            </Col>
+          </Row>
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </Row>
+          <button className="btn btn-success" type="submit">
+            Submit
+          </button>
+        </Container>
+        <Container className="mt-4">
+          <h3>Name: {username}</h3>
+          <p>Password: {password}</p>
+        </Container>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
